a8460dce252a5c8803868279420cc907890f393c
moawia-dev <moawia.asali@gmail.com>
2021-04-04 13:16:30 +0200

    change to class
    change App function to App class

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './App.css';
 import PokedexHeader from'./components/PokedexHeader'
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "Maan"
+    }
+  }
+  
+  
+  render() {
   return (
     <div className="App">
-      <PokedexHeader name={"Moawia"} author={'Mimo'} />
+      <PokedexHeader name={this.state.name} author={'Mimo'} />
     </div>
   );
 }
-
+}
 export default App;
--- src/components/PokedexHeader.js
+++ src/components/PokedexHeader.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {
     Header,
-    Image,
+
     Segment
 } from 'semantic-ui-react'
+import { Image } from 'semantic-ui-react'
 
 const PokedexHeader = ({author, name}) => {
   return(
       <Segment basic inverted padded='very' vertical>
     <Header as='h1'>This is a Header for {name} Pokedex!</Header>
-    <Image src={'./src/Images/image.png'}  />      
+    <Image src={'Maan.jpg'} alt="Maan" centered size={'small'} />      
     <Header as='h3'>Author: {author}</Header>
     
     </Segment>
