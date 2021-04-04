efcc20978c4ba9400d892063ec1f164a68675761
moawia-dev <moawia.asali@gmail.com>
2021-04-04 10:19:41 +0200

    Install React Semantic UI
    semantic-ui-react and semantic-ui-css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Hello world and fuck you
-        </p>
+        </h1>
         <p>:)</p>
         <a
           className="App-link"
--- src/index.js
+++ src/index.js
@@ -3,11 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css'
+const Foo = ({ name }) => {
+  return (
+    
+    <p>This is a component. It's name is {name}</p>
+  )
+}
 
-ReactDOM.render(
+const Item = (props) => {
+  return (
+    <div>
+    <p>This is our first component. it is name is {props.name}</p>
+    <Foo name={'Maria'}/>
+    {item}
+    </div>
+  )
+}
+
+const item = <p>This is an item </p>
+const element = <div><h1>Hello, element</h1>
+ <p>This is a test</p>
+ {item}
+ <Item name={'Moawia'}/>
+ <Item name={'Kajsa'}/>
+ 
+</div> 
+ReactDOM.render(  
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
+    element,
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
