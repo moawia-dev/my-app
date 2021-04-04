c26664814882395263268b54a82a9ee553c3a50b
moawia-dev <moawia.asali@gmail.com>
2021-04-04 13:52:28 +0200

    added an input tied to the state
    this.onChange = this.onChange.bind(this)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import './App.css';
 import PokedexHeader from'./components/PokedexHeader'
-
+import {
+  Input,
+  Segment
+} from 'semantic-ui-react'
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      author: 'Mimo',
       name: "Maan"
     }
+    this.onChange = this.onChange.bind(this)
+  }
+  onChange(event, data){
+    this.setState({
+      name: data.value
+      
+    })
   }
   
-  
   render() {
+    console.log(this.state)
   return (
     <div className="App">
-      <PokedexHeader name={this.state.name} author={'Mimo'} />
+      <PokedexHeader name={this.state.name} author={this.state.author} />
+      <Segment basic>
+        <Input 
+        label={"Input Form"}
+        onChange={this.onChange}
+        value={this.state.name}
+        />
+      </Segment>
+      
     </div>
   );
 }
